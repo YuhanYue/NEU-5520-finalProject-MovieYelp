a76c45d8c04877414bf6af9783d5e61c712a4eb9
Yun Feng <iostream3100@gmail.com>
2021-11-28 14:37:06 -0800

    Implemented fetching movies from database

--- MovieYelp/screens/Profile.js
+++ MovieYelp/screens/Profile.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, SafeAreaView, StyleSheet, FlatList, Text } from 'react-native';
+import React from "react";
+import { View, SafeAreaView, StyleSheet, FlatList, Text } from "react-native";
 // import {
 //   Orange: #F97878
 //   Pink: #ea3372
@@ -11,53 +11,69 @@ import { View, SafeAreaView, StyleSheet, FlatList, Text } from 'react-native';
 // import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // import Share from 'react-native-share';
-import { ScrollView, Alert, TouchableOpacity, Image, Button } from 'react-native';
-import { TouchableOpacityBase } from 'react-native';
-import { Component } from 'react';
-import Card from '../components/Card'
-import { LinearGradient } from 'expo-linear-gradient';
-import { block } from 'react-native-reanimated';
-import { Ionicons, Entypo } from '@expo/vector-icons';
+import {
+  ScrollView,
+  Alert,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
+import { TouchableOpacityBase } from "react-native";
+import { Component } from "react";
+import Card from "../components/Card";
+import { LinearGradient } from "expo-linear-gradient";
+import { block } from "react-native-reanimated";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import firebase from "../firbase-config";
 
 const userName = "Dog Lover";
 const userEmail = "myemail@email.com";
-const profilePic = 'https://picsum.photos/id/200/200/200';
-const userBio = '"Hello, I am a student from Northeastern University and I like movies, hit me up if you like movies too! "';
-const displayPics = ['https://picsum.photos/id/500/200/200', 'https://picsum.photos/id/600/200/200', 'https://picsum.photos/id/700/200/200', 'https://picsum.photos/id/800/200/200', 'https://picsum.photos/id/900/200/200', 'https://picsum.photos/id/377/200/200']
+const profilePic = "https://picsum.photos/id/200/200/200";
+const userBio =
+  '"Hello, I am a student from Northeastern University and I like movies, hit me up if you like movies too! "';
+const displayPics = [
+  "https://picsum.photos/id/500/200/200",
+  "https://picsum.photos/id/600/200/200",
+  "https://picsum.photos/id/700/200/200",
+  "https://picsum.photos/id/800/200/200",
+  "https://picsum.photos/id/900/200/200",
+  "https://picsum.photos/id/377/200/200",
+];
 
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    var ref = firebase.database().ref("users/ada");
+    ref.this.state = {
       pairedImageData: null,
       dehazeCount: 0,
       postPress: true,
       galleryPress: false,
-    }
+    };
   }
 
   morePress = () => {
     // console.log("You press the button");
-    Alert.alert('MORE', "What's more?", [
+    Alert.alert("MORE", "What's more?", [
       { text: "Edit profile" },
       { text: "Open Setting" },
-      { text: "Close" }
+      { text: "Close" },
     ]);
-  }
+  };
 
   myPostBress = () => {
     this.setState({
       postPress: true,
       galleryPress: false,
-    })
-  }
+    });
+  };
 
   myGalleryBress = () => {
     this.setState({
       postPress: false,
       galleryPress: true,
-    })
-  }
+    });
+  };
 
   postPics = () => {
     var res = displayPics;
@@ -65,55 +81,111 @@ export default class Profile extends React.Component {
     if (this.state.postPress) {
       for (var i = 0; i < res.length; i++) {
         table.push(
-          <View key={i.toString()} style={{ flexDirection: 'row' }}>
-            {<View style={{ flex: 1, marginTop: 10 }}><Text style={{ color: "grey" }}>MM/DD/YY</Text></View>}
-            {<View style={{ flex: 4 }} >
-              {<Image source={{
-                uri: res[i]
-              }} style={{ width: 250, height: 250, borderRadius: 10, marginTop: 10 }} />}
-            </View>}
-          </View>)
+          <View key={i.toString()} style={{ flexDirection: "row" }}>
+            {
+              <View style={{ flex: 1, marginTop: 10 }}>
+                <Text style={{ color: "grey" }}>MM/DD/YY</Text>
+              </View>
+            }
+            {
+              <View style={{ flex: 4 }}>
+                {
+                  <Image
+                    source={{
+                      uri: res[i],
+                    }}
+                    style={{
+                      width: 250,
+                      height: 250,
+                      borderRadius: 10,
+                      marginTop: 10,
+                    }}
+                  />
+                }
+              </View>
+            }
+          </View>
+        );
       }
-
     }
     return table;
-  }
+  };
 
   galleryPics = () => {
-
     var res = displayPics;
 
-    var ans = []
+    var ans = [];
     if (this.state.galleryPress) {
       for (var i = 0; i < res.length; i += 2) {
         var table = [];
         if (i + 1 < res.length) {
           table.push(
-            <View key={i.toString()} style={{ flexDirection: 'row', alignItems: 'center' }}>
-              {<Image source={{
-                uri: res[i]
-              }} style={{ width: 175, height: 175, borderRadius: 10, margin: 10 }} />}
-              {<Image source={{
-                uri: res[i + 1]
-              }} style={{ width: 175, height: 175, borderRadius: 10, margin: 10 }} />}
+            <View
+              key={i.toString()}
+              style={{ flexDirection: "row", alignItems: "center" }}
+            >
+              {
+                <Image
+                  source={{
+                    uri: res[i],
+                  }}
+                  style={{
+                    width: 175,
+                    height: 175,
+                    borderRadius: 10,
+                    margin: 10,
+                  }}
+                />
+              }
+              {
+                <Image
+                  source={{
+                    uri: res[i + 1],
+                  }}
+                  style={{
+                    width: 175,
+                    height: 175,
+                    borderRadius: 10,
+                    margin: 10,
+                  }}
+                />
+              }
             </View>
-          )
+          );
         } else {
           table.push(
-            <View key={i.toString()} style={{ flexDirection: 'row', alignItems: 'center' }}>
-              {<Image source={{
-                uri: res[i]
-              }} style={{ width: 175, height: 175, borderRadius: 10, margin: 10 }} />}
+            <View
+              key={i.toString()}
+              style={{ flexDirection: "row", alignItems: "center" }}
+            >
+              {
+                <Image
+                  source={{
+                    uri: res[i],
+                  }}
+                  style={{
+                    width: 175,
+                    height: 175,
+                    borderRadius: 10,
+                    margin: 10,
+                  }}
+                />
+              }
             </View>
-          )
+          );
         }
-        ans.push(<View key={(i * 100).toString()} style={{ flexDirection: 'row', alignContent: 'center' }}>{table}</View>)
+        ans.push(
+          <View
+            key={(i * 100).toString()}
+            style={{ flexDirection: "row", alignContent: "center" }}
+          >
+            {table}
+          </View>
+        );
       }
-
     }
     return ans;
-
-  }
+  };
 
   render() {
     var displayView;
@@ -128,22 +200,27 @@ export default class Profile extends React.Component {
 
     // this.fetchData();
     return (
-
       // user info part
       <View>
         {/*This is the header: */}
         <View>
           {/* <LinearGradient colors={['#61698E', '#F97878']} start={[0.5, 0.5]} end={[0.5, 0.5]}> */}
-            <View style={{ marginHorizontal: 60, paddingVertical: 20, alignItems: 'center' }}>
-              {/* <Text> This is a profile page</Text> */}
-              <TouchableOpacity>
-                <Image
-                  source={{ uri: profilePic }}
-                  style={styles.avatar} />
-              </TouchableOpacity>
-              <Text style={styles.userName}>{userName}</Text>
-              <Text style={{ color: "white", textAlign: 'center' }}>{userEmail}</Text>
-            </View>
+          <View
+            style={{
+              marginHorizontal: 60,
+              paddingVertical: 20,
+              alignItems: "center",
+            }}
+          >
+            {/* <Text> This is a profile page</Text> */}
+            <TouchableOpacity>
+              <Image source={{ uri: profilePic }} style={styles.avatar} />
+            </TouchableOpacity>
+            <Text style={styles.userName}>{userName}</Text>
+            <Text style={{ color: "white", textAlign: "center" }}>
+              {userEmail}
+            </Text>
+          </View>
           {/* </LinearGradient> */}
           <View style={{ marginHorizontal: 10, paddingVertical: 10 }}>
             <Text style={{ fontWeight: "600", textAlign: "left" }}>
@@ -153,27 +230,65 @@ export default class Profile extends React.Component {
               {userBio}
             </Text>
             <View>
-              <Ionicons name="location-outline" style={{ color: "grey", padding: 10 }}>Vancouver, Birtish Columbia</Ionicons>
+              <Ionicons
+                name="location-outline"
+                style={{ color: "grey", padding: 10 }}
+              >
+                Vancouver, Birtish Columbia
+              </Ionicons>
             </View>
           </View>
 
           {/*This is the status bar: */}
           <View style={{ flexDirection: "row" }}>
-            <View style={{ flex: 1, alignItems: "center", borderWidth: 1, borderColor: "lightgrey" }}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "lightgrey",
+              }}
+            >
               <TouchableOpacity onPress={this.myPostBress}>
-                <Ionicons name="add-circle" style={{ color: "#FF8000" }} size={24} />
+                <Ionicons
+                  name="add-circle"
+                  style={{ color: "#FF8000" }}
+                  size={24}
+                />
               </TouchableOpacity>
               <Text style={{ color: "grey" }}>My Post</Text>
             </View>
-            <View style={{ flex: 1, alignItems: "center", borderWidth: 1, borderColor: "lightgrey" }}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "lightgrey",
+              }}
+            >
               <TouchableOpacity onPress={this.myGalleryBress}>
-                <Ionicons name="images" style={{ color: "#FF8000" }} size={24} />
+                <Ionicons
+                  name="images"
+                  style={{ color: "#FF8000" }}
+                  size={24}
+                />
               </TouchableOpacity>
               <Text style={{ color: "grey" }}>My Gallery</Text>
             </View>
-            <View style={{ flex: 1, alignItems: "center", borderWidth: 1, borderColor: "lightgrey" }}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "lightgrey",
+              }}
+            >
               <TouchableOpacity onPress={this.morePress}>
-                <Ionicons name="ellipsis-horizontal" style={{ color: "#FF8000" }} size={24} />
+                <Ionicons
+                  name="ellipsis-horizontal"
+                  style={{ color: "#FF8000" }}
+                  size={24}
+                />
               </TouchableOpacity>
               <Text style={{ color: "grey" }}>More</Text>
             </View>
@@ -182,27 +297,20 @@ export default class Profile extends React.Component {
           {/*This is the display/post view: */}
           <View style={{ margin: 2, borderColor: "grey" }}>
             <ScrollView>
-              <View style={{ height: height }}>
-                {displayView}
-              </View>
+              <View style={{ height: height }}>{displayView}</View>
             </ScrollView>
-
           </View>
         </View>
-
       </View>
-
     );
   }
-
-};
-
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   userInfoSection: {
     paddingHorizontal: 30,
@@ -210,49 +318,47 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   caption: {
     fontSize: 14,
     lineHeight: 14,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 10,
   },
   infoBoxWrapper: {
-    borderBottomColor: '#dddddd',
+    borderBottomColor: "#dddddd",
     borderBottomWidth: 1,
-    borderTopColor: '#dddddd',
+    borderTopColor: "#dddddd",
     borderTopWidth: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
 
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   infoBox: {
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  avatar:{
+  avatar: {
     width: 100,
-    height:100,
+    height: 100,
     borderRadius: 100,
   },
-  userTitle:{
+  userTitle: {
     fontSize: 16,
-    color:"#b8bece",
-    fontWeight:"500",
-    justifyContent: 'center',
+    color: "#b8bece",
+    fontWeight: "500",
+    justifyContent: "center",
   },
-  userName:{
-    fontSize:20,
-    color:"#3c4560",
-    fontWeight: 'bold',
+  userName: {
+    fontSize: 20,
+    color: "#3c4560",
+    fontWeight: "bold",
     marginLeft: 0,
-    
   },
-
 });
--- MovieYelp/screens/SignUp.js
+++ MovieYelp/screens/SignUp.js
@@ -1,176 +1,220 @@
-import React from 'react';
+import React from "react";
 import {
-    View,
-    StyleSheet,
-    Text,
-    ScrollView,
-    Image,
-    TouchableOpacity
-} from 'react-native'
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-import Submit from '../components/Submit'
-import Inputs from '../components/Inputs'
-import { auth } from '../firbase-config';
-import { createUserWithEmailAndPassword } from '@firebase/auth';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {Input} from 'react-native-elements'
+import Submit from "../components/Submit";
+import Inputs from "../components/Inputs";
+import { auth } from "../firbase-config";
+import { createUserWithEmailAndPassword } from "@firebase/auth";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input } from "react-native-elements";
+import { firebase } from "../firbase-config";
+// import { firestore } from "@firebase/firestore";
+// import database from "@react-native-firebase/database";
+// import firestore from "@react-native-firebase/firestore";
 
 export default class SignUp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userEmail:"",
-            userPassword:"",
-            isFocused: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userEmail: "",
+      userPassword: "",
+      isFocused: false,
+    };
+  }
+
+  onFoucusChange = () => {
+    this.setState({ isFocused: true });
+  };
+
+  handleRegister = async () => {
+    try {
+      // const user = createUserWithEmailAndPassword(
+      //   auth,
+      //   this.state.userEmail,
+      //   this.state.userPassword
+      // );
+      // console.log(user);
+
+      createUserWithEmailAndPassword(
+        auth,
+        this.state.userEmail,
+        this.state.userPassword
+      ).then((response) => {
+        const uid = response.user.uid;
+        const data = {
+          id: uid,
+          email: this.state.userEmail,
+          // fullName,
+        };
+        console.log(data);
+        const usersRef = firestore()
+          .collection("users")
+          .doc("5N4QYz7mpz6C0JsPxOsp");
+        console.log(usersRef);
+        // usersRef
+        //   .doc(firestore, uid)
+        //   .set(data)
+        //   .catch((error) => {
+        //     alert(error);
+        //   });
+      });
+    } catch {
+      console.log(Error);
     }
+  };
 
+  onChangePassword = (password) => {
+    this.setState({
+      userPassword: password,
+    });
+  };
 
-    onFoucusChange = () =>{
-        this.setState({isFocused: true})
-    }
+  onChangeUsername = (email) => {
+    this.setState({
+      userEmail: email,
+    });
+  };
 
-    handleRegister = async() =>{
-        try{            
-            const user = 
-            createUserWithEmailAndPassword(auth,this.state.userEmail, this.state.userPassword);
-            console.log(user);
-        }catch{
-            console.log(Error);
-        }
+  render() {
+    return (
+      <ScrollView style={{ backgroundColor: "white" }}>
+        <View style={styles.container}>
+          <Image
+            source={require("../assets/registeration.png")}
+            style={styles.image}
+          />
+          <Text style={styles.textTitle}>Let's Get Started</Text>
+          <Text style={styles.textBody}>
+            Create a new account for your dehaze APP
+          </Text>
+          {/* <Inputs name='Full Name' icon='user'/> */}
+          <View
+            style={[
+              styles.inputContainerWrap,
+              { borderColor: this.state.isFocused ? "#0779ef" : "#eee" },
+            ]}
+          >
+            <Input
+              onChangeText={this.onChangeUsername}
+              placeholder="Email"
+              onFocus={this.onFoucusChange}
+              inputContainerStyle={styles.inputContainer}
+              inputSyle={styles.inputText}
+              secureTextEnrty="false"
+              leftIcon={
+                <Icon
+                  name="envelope"
+                  size={20}
+                  color={this.state.isFocused ? "#0779e4" : "grey"}
+                />
+              }
+              // onChangeText = {this.passTextToFather}
+            />
+          </View>
+          {/* <Inputs name='Phone' icon='phone'/> */}
 
-    }
-
-    onChangePassword = (password) =>{
-        this.setState({
-            userPassword: password
-        })
-        
-    }
-
-
-    onChangeUsername = (email) =>{
-        this.setState({
-            userEmail: email
-        })
-    }
-
-    render(){
-        return(
-            <ScrollView style={{backgroundColor: 'white'}}>
-                <View style={styles.container}>
-                    <Image source={require('../assets/registeration.png')}
-                    style={styles.image}/> 
-                     <Text style={styles.textTitle}>Let's Get Started</Text>
-                     <Text style={styles.textBody}>Create a new account for your dehaze APP</Text>
-                     {/* <Inputs name='Full Name' icon='user'/> */}
-                     <View 
-            style = {[styles.inputContainerWrap, {borderColor: this.state.isFocused ? '#0779ef':'#eee'}]}>
-                <Input
-                onChangeText={this.onChangeUsername}
-                    placeholder = "Email"
-                    onFocus = {this.onFoucusChange}
-                    inputContainerStyle = {styles.inputContainer}
-                    inputSyle = {styles.inputText}
-                    secureTextEnrty ="false"
-                    leftIcon = {
-                        <Icon 
-                            name="envelope"
-                            size={20}
-                            color={this.state.isFocused ? '#0779e4':'grey'}
-                            />
-                    }
-                    // onChangeText = {this.passTextToFather}
-                    />
-            </View>
-                     {/* <Inputs name='Phone' icon='phone'/> */}
-
-                     <View 
-            style = {[styles.inputContainerWrap, {borderColor: this.state.isFocused ? '#0779ef':'#eee'}]}>
-                <Input
-                onChangeText={this.onChangePassword}
-                    placeholder = "Password"
-                    onFocus = {this.onFoucusChange}
-                    inputContainerStyle = {styles.inputContainer}
-                    inputSyle = {styles.inputText}
-                    secureTextEnrty ="false"
-                    leftIcon = {
-                        <Icon 
-                            name="lock"
-                            size={20}
-                            color={this.state.isFocused ? '#0779e4':'grey'}
-                            />
-                    }
-                    // onChangeText = {this.passTextToFather}
-                    />
-            </View>
-                     {/* <Inputs name='Confirm Password' icon='lock' pass={true} /> */}
-                     <TouchableOpacity style={[styles.submitContainer, {backgroundColor: 'blue'}]}
-                     onPress = {this.handleRegister}>
-            <Text style = {styles.submitText}>Create</Text>
-        </TouchableOpacity>
-                     <View style={{flexDirection: 'row'}}>
-                         <Text style={styles.textBody}>Already have an account</Text>
-                         <Text style={[styles.textBody, {color:'blue'}]} onPress={(  /*括号后面不能加空格 unexpected token*/
-                         ) => this.props.navigation.navigate('Login')}> Login Here</Text>
-                     </View>
-                </View>
-               
-            </ScrollView>
-        );
-    }
-    
-};
+          <View
+            style={[
+              styles.inputContainerWrap,
+              { borderColor: this.state.isFocused ? "#0779ef" : "#eee" },
+            ]}
+          >
+            <Input
+              onChangeText={this.onChangePassword}
+              placeholder="Password"
+              onFocus={this.onFoucusChange}
+              inputContainerStyle={styles.inputContainer}
+              inputSyle={styles.inputText}
+              secureTextEnrty="false"
+              leftIcon={
+                <Icon
+                  name="lock"
+                  size={20}
+                  color={this.state.isFocused ? "#0779e4" : "grey"}
+                />
+              }
+              // onChangeText = {this.passTextToFather}
+            />
+          </View>
+          {/* <Inputs name='Confirm Password' icon='lock' pass={true} /> */}
+          <TouchableOpacity
+            style={[styles.submitContainer, { backgroundColor: "blue" }]}
+            onPress={this.handleRegister}
+          >
+            <Text style={styles.submitText}>Create</Text>
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.textBody}>Already have an account</Text>
+            <Text
+              style={[styles.textBody, { color: "blue" }]}
+              onPress={() =>
+                /*括号后面不能加空格 unexpected token*/
+                this.props.navigation.navigate("Login")
+              }
+            >
+              {" "}
+              Login Here
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-
-    container:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    image:{
-        width: 348,
-        height: 300,
-        marginVertical: 50
-    },
-    textTitle:{
-        fontWeight:'bold',
-        fontSize:40,
-    },
-    textBody:{
-        fontSize: 16,
-    
-    },
-    submitContainer: {
-        width: '90%',
-        height: 50,
-        borderColor: 'blue',
-        borderRadius: 10,
-        marginVertical: 10,
-        borderWidth: 0,
-    },
-    submitText: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        color: 'white',
-        alignSelf:'center',
-        marginVertical: 10,
-    },
-    inputContainerWrap:{
-        width: '90%',
-        height: 50,
-        borderRadius: 100,
-        marginVertical:15,
-        borderWidth: 3.5,
-    },
-    inputContainer: {
-        borderBottomWidth: 0
-    },
-    inputText: {
-        color:'#0779e4',
-        fontWeight: 'bold',
-        marginLeft: 5,
-    }
-    
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 348,
+    height: 300,
+    marginVertical: 50,
+  },
+  textTitle: {
+    fontWeight: "bold",
+    fontSize: 40,
+  },
+  textBody: {
+    fontSize: 16,
+  },
+  submitContainer: {
+    width: "90%",
+    height: 50,
+    borderColor: "blue",
+    borderRadius: 10,
+    marginVertical: 10,
+    borderWidth: 0,
+  },
+  submitText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "white",
+    alignSelf: "center",
+    marginVertical: 10,
+  },
+  inputContainerWrap: {
+    width: "90%",
+    height: 50,
+    borderRadius: 100,
+    marginVertical: 15,
+    borderWidth: 3.5,
+  },
+  inputContainer: {
+    borderBottomWidth: 0,
+  },
+  inputText: {
+    color: "#0779e4",
+    fontWeight: "bold",
+    marginLeft: 5,
+  },
 });
