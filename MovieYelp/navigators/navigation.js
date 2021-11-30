81532c84ba598fa7e39628a980829d39150b6595
jiabin123 <jiabinx@uci.edu>
2021-11-30 13:46:17 -0800

    Update navigation.js

--- MovieYelp/navigators/navigation.js
+++ MovieYelp/navigators/navigation.js
@@ -1,29 +1,39 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack'
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // import {createAppContainer } from "react-navigation";
 // import {createStackNavigator} from 'react-navigation-stack'//how many screens you wanna have in a stack
 
-import SignUp from '../screens/SignUp';
-import Login from "../screens/Login"
-import HomePage from '../screens/HomePage';
-import TabNavigator from './tabNavigator';
+import SignUp from "../screens/SignUp";
+import Login from "../screens/Login";
+import HomePage from "../screens/HomePage";
+import TabNavigator from "./tabNavigator";
 
-
-const Stack = createStackNavigator();//main stack for login/signUp/User's Homepage
-
+const Stack = createStackNavigator(); //main stack for login/signUp/User's Homepage
 function Navigation() {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName = "Login">
-                <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
-                <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}} />
-                <Stack.Screen name="HomePageNavigation" component = {TabNavigator} options={{headerShown:false}}/>
-                {/* <Stack.Screen name="HomePage" component={MainNavigation} options={{headerShown:false}}/> */}
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomePageNavigation"
+          component={TabNavigator}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen name="HomePage" component={MainNavigation} options={{headerShown:false}}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default Navigation;
